8cfebad8ade0bb302b3b9f238ccfffced779a130
Subharaj Chaudhary <subharazzchy68@gmail.com>
2022-12-25 12:21:38 +0545

    remove header tsx and update sidebar

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <div className='wrapper'>
+    <div className='wrapper' style={{paddingTop: "0.3rem"}}>
       <div className='container '>
         <div className='header'>
           <div className='logo'>
-            <h3>SNC</h3>
+            <h3 style={{height:"32px"}}>SNC</h3>
           </div>
         </div>
       </div>
